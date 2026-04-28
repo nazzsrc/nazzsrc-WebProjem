1790c35481a0faba072e3b4386a4e474cc312791
nazzsrc <nazsarica05@gmail.com>
2026-04-28 16:24:38 +0300

    ilk sayfalar ve Bootstrap iskeleti oluşturuldu

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Teknolojileri Projesi | Hakkımda</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">B241210063</a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Hakkımda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="cv.php">Özgeçmiş</a></li>
+                    <li class="nav-item"><a class="nav-link" href="sehrim.php">Şehrim</a></li>
+                    <li class="nav-item"><a class="nav-link" href="mirasimiz.php">Mirasımız</a></li>
+                    <li class="nav-item"><a class="nav-link" href="ilgi-alanlarim.php">İlgi Alanlarım</a></li>
+                    <li class="nav-item"><a class="nav-link" href="iletisim.php">İletişim</a></li>
+                    <li class="nav-item"><a class="btn btn-outline-info ms-lg-3" href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container my-5">
+        <div class="row align-items-center">
+            <div class="col-md-5">
+                <img src="https://via.placeholder.com/400x400" class="img-fluid rounded shadow-lg" alt="Profil Resmim">
+            </div>
+            <div class="col-md-7">
+                <h2 class="display-4">Merhaba, Ben [Adın Soyadın]</h2>
+                <p class="lead mt-3">Sakarya Üniversitesi, Bilgisayar Mühendisliği öğrencisiyim.</p>
+                <p>Boş zamanlarımda kitap okumayı, yeni web teknolojilerini araştırmayı ve doğa yürüyüşlerini severim. Bu web sitesi, Web Teknolojileri dersi kapsamında geliştirdiğim ilk profesyonel uygulamadır.</p>
+                
+                <div class="mt-4">
+                    <h5>Hobilerim:</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">💻 Kodlama ve Proje Geliştirme</li>
+                        <li class="list-group-item">🎸 Müzik Dinlemek</li>
+                        <li class="list-group-item">📸 Fotoğrafçılık</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
